357780d2b86b647a16f1ba694cb6876990c99179
bentok <bscopeland@gmail.com>
2022-09-20 19:17:49 -0400

    Godot project and FSharp scripting project

---
+++ FSharpScripting/Library.fs
@@ -0,0 +1,13 @@
+﻿namespace Player
+
+open Godot
+
+type StaticBody2d () =
+    inherit StaticBody2D()
+    
+    override this._Ready() =
+        ()
+        
+    override this._Process(delta) =
+        printfn $"%A{delta}"
+        ()
---
+++ StaticBody2d.cs
@@ -0,0 +1,5 @@
+using Godot;
+
+using Player;
+
+public partial class StaticBody2dCS : StaticBody2d  { }
